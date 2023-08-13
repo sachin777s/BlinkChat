60c2056e67396b4b0f43e8cba3d18076a8999ecb
Sachin Kumar <96292115+sachin777s@users.noreply.github.com>
2023-08-13 11:45:27 +0530

    Updated front end url

--- server/app.js
+++ server/app.js
@@ -15,6 +15,13 @@ env.config();
 const app = express();
 
 /* Middlewares */
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://blinkchats.netlify.app/'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(
   cors({
     credentials: true,
