01536a8b0efb945a363271500a609f724f434686
Sachin Kumar <96292115+sachin777s@users.noreply.github.com>
2023-08-29 11:35:42 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Origin', "https://blinkchats.netlify.app");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
   next();
