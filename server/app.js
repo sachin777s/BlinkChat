d4ec98a5c413184948de8bbe2fcfa1a56dbf5c17
Sachin Kumar <96292115+sachin777s@users.noreply.github.com>
2023-08-29 11:28:07 +0530

    Updated aceess controll credentials

--- server/app.js
+++ server/app.js
@@ -14,6 +14,14 @@ const PORT = process.env.PORT || 8000;
 env.config();
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
+
 /* Middlewares */
 app.use(
   cors({
